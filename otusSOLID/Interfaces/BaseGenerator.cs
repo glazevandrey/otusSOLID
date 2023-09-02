90e9ed38d12b9f6f939845a4d84b186dbfea03c6
aglazev <andreiglazev@mail.ru>
2023-09-02 18:14:56 +0300

    а

--- otusSOLID/Interfaces/BaseGenerator.cs
+++ otusSOLID/Interfaces/BaseGenerator.cs
@@ -5,12 +5,14 @@ namespace otusSOLID.Interfaces
     public abstract class BaseGenerator
     {
         public BaseSettings GeneratorSettings { get; private set; }
-        public abstract int GetRandomNumber();
 
         public BaseGenerator(BaseSettings settings)
         {
                 GeneratorSettings = settings;
         }
+
+        public abstract int GetRandomNumber();
+
 
     }
 }
